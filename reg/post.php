85a8f2d5f7d5f219bf88f984e809f3f7bf6750ff
CubeStudio001 <cubemode@163.com>
2022-08-04 10:05:18 +0800

    405 fixed

--- reg/post.php
+++ reg/post.php
@@ -1,17 +1,13 @@
 <?php
 ob_start();
-if($_GET["email"]==null){
-    echo "发生错误！点击<a href=\"../\">这里</a>的链接返回主页";
-    die();
-}
-echo "邮箱地址",$_GET["email"],"已被注册,即将将您送回首页";
 
-ob_end_flush();
+echo "您的邮箱已被注册,即将将您送回首页";
+
+
 
 sleep(3);
 
+ob_end_flush();
 
 header("Location: https://www.bilibili.com/video/BV1GJ411x7h7");
 ?>
-
-<a href="../">Go Back</a>
