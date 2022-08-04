17bcda9ebbd25033d25a7932cbb25c2cbcb60f65
CubeStudio001 <cubemode@163.com>
2022-08-04 09:44:35 +0800

    405 fixed

--- reg/post.php
+++ reg/post.php
@@ -1,10 +1,10 @@
 <?php
 ob_start();
-if($_POST["email"]==null){
+if($_GET["email"]==null){
     echo "发生错误！点击<a href=\"../\">这里</a>的链接返回主页";
     die();
 }
-echo "邮箱地址",$_POST["email"],"已被注册,即将将您送回首页";
+echo "邮箱地址",$_GET["email"],"已被注册,即将将您送回首页";
 
 ob_end_flush();
 
