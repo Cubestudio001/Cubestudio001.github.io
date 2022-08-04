02fa87df0a14332876488960ccb5822e27e8d323
CubeStudio001 <cubemode@163.com>
2022-08-04 09:41:20 +0800

    JUDGE ENGINE GREATE UPDATE

---
+++ reg/post.php
@@ -0,0 +1,17 @@
+<?php
+ob_start();
+if($_POST["email"]==null){
+    echo "发生错误！点击<a href=\"../\">这里</a>的链接返回主页";
+    die();
+}
+echo "邮箱地址",$_POST["email"],"已被注册,即将将您送回首页";
+
+ob_end_flush();
+
+sleep(3);
+
+
+header("Location: https://www.bilibili.com/video/BV1GJ411x7h7");
+?>
+
+<a href="../">Go Back</a>
